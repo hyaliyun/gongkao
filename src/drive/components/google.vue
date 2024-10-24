59a3200299718b3036a764352b834dbdb87ec95e
react <hy0228@aliyun.com>
2024-10-24 14:33:34 +0800

    Add files via upload

---
+++ src/drive/components/google.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="exam-cheer">
+    公考加油！
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExamCheer',
+
+}
+</script>
+
+<style scoped>
+.exam-cheer {
+  font-size: 24px;
+  color: #008000; /* 深绿色，寓意希望和成功 */
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
